dacec1eab0fc486c05e01b70f1f29c4119f086b3
Lukas Elmer <lukas.elmer@gmail.com>
2017-07-07 17:39:48 +0200

    Config SSL for development

---
+++ ssl/config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+openssl genrsa -des3 -out rootCA.key 2048
+chmod 600 rootCA.key
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem
+openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config <( cat server.csr.cnf )
+chmod 600 server.key
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext
+
+echo 'Checking if sanhei-bills.dev is in /etc/hosts'
+grep 'sanhei-bills.dev' /etc/hosts || echo '127.0.0.1 sanhei-bills.dev' | sudo tee -a /etc/hosts
+
+echo 'Installing certificate'
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain rootCA.pem
