5ab437daa04e93a1ed35243371f226b875902f47
Lukas Elmer <lukas.elmer@gmail.com>
2017-07-07 12:59:47 +0200

    Angular scaffold

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SanheiBills2Page } from './app.po';
+
+describe('sanhei-bills2 App', () => {
+  let page: SanheiBills2Page;
+
+  beforeEach(() => {
+    page = new SanheiBills2Page();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
