ba299fa0e3a3ddbbf7bf9b92823dee23bba5ad33
Lukas Elmer <lukas.elmer@gmail.com>
2017-07-15 00:31:54 +0200

    Merge origin/master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { MaterialModule, MdButtonModule, MdCardModule, MdCheckbox, MdList, MdListItem, MdProgressSpinnerModule, MdProgressSpinnerModule } from '@angular/material';
+import { MaterialModule, MdButtonModule, MdCardModule, MdCheckbox, MdList, MdListItem, MdProgressSpinnerModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
