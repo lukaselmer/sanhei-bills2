d8f9dd0269c4b806dd79d0f88ae6da3ed3932534
Lukas Elmer <lukas.elmer@gmail.com>
2017-07-18 23:36:50 +0200

    Fix tests, cleanup

--- src/app/bills/bills-list/bills-list.component.spec.ts
+++ src/app/bills/bills-list/bills-list.component.spec.ts
@@ -1,44 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MaterialModule, MdCheckboxModule, MdListModule } from '@angular/material';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { MdCheckboxModule, MdInputModule, MdListModule, MdProgressBarModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 import 'rxjs/add/observable/of';
 import { Observable } from 'rxjs/Observable';
 import { BillsService } from '../bills.service';
+import { SearchResult } from '../search/search-result';
+import { Bill } from './../bill';
 import { BillsListComponent } from './bills-list.component';
 
 describe('BillsListComponent', () => {
   let component: BillsListComponent;
   let fixture: ComponentFixture<BillsListComponent>;
-  const bills = [
-    {
-      id: '1234',
-      uid: '17071234',
-      address1: 'Adresszeile 1',
-      address2: 'Adressezeile 2',
-      title1: 'Objekt: Adresse',
-      title2: 'Zusatz'
-    }
-  ];
+  const bill: Bill = {
+    id: 1234,
+    uid: '17071234',
+    address1: 'Adresszeile 1',
+    address2: 'Adressezeile 2',
+    title1: 'Objekt: Adresse',
+    title2: 'Zusatz'
+  } as any;
+  const bills = [bill];
+  const billsSearch = {
+    term: '',
+    list: bills,
+    dbStatus: 'loaded' as 'loaded'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        MdCheckboxModule,
+        MdInputModule,
         MdListModule,
-        MdCheckboxModule
+        MdProgressBarModule,
+        NoopAnimationsModule
       ],
       providers: [
-        { provide: BillsService, useValue: { forIndex: () => Observable.of(bills) } }
+        { provide: BillsService, useValue: { search: (): Observable<SearchResult<Bill>> => Observable.of(billsSearch) } }
       ],
       declarations: [BillsListComponent]
     }).compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(fakeAsync(() => {
     fixture = TestBed.createComponent(BillsListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    tick(100);
+  }));
 
   it('should be created', () => {
     expect(component).toBeTruthy();
@@ -50,19 +61,18 @@ describe('BillsListComponent', () => {
     expect(compiled.querySelector('md-list')).not.toBe(null);
   }));
 
-  it('should render a row for each bill', async(() => {
+  it('should render a row for each bill', fakeAsync(() => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('md-list > md-list-item').childElementCount).toBe(bills.length);
   }));
 
-  it('should render the row for the bill', async(() => {
+  it('should render the row for the bill', fakeAsync(() => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    const bill = bills[0];
-    const element = compiled.querySelector(`md-list-item#bill_${bill.id}`);
+    const element = compiled.querySelector(`md-list-item`);
     expect(element).not.toBe(null);
-    expect(element.querySelector('[md-line]:nth-child(2)').textContent.trim()).toEqual(bill.id);
+    expect(element.querySelector('[md-line]:nth-child(2)').textContent.trim()).toEqual(bill.id.toString());
 
     const billTitle = `${bill.uid} ${bill.address1}, ${bill.address2}, ${bill.title1}, ${bill.title2}`;
     expect(element.querySelector('[md-line]:nth-child(3)').textContent.trim()).toEqual(billTitle);
