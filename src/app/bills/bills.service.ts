fdec85f5d572194a60ab77dbc3a30ce31fa739cb
Lukas Elmer <lukas.elmer@gmail.com>
2017-07-19 03:58:28 +0200

    Extract data storage, add indexedDB cache

--- src/app/bills/bills.service.ts
+++ src/app/bills/bills.service.ts
@@ -8,40 +8,23 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Bill } from './bill';
 import { SearchResult } from './search/search-result';
+import { DataStoreService } from './store/data-store.service';
 
 @Injectable()
 export class BillsService {
-  private billsCache = new BehaviorSubject<Bill[]>([]);
-  private status: 'loading' | 'partiallyLoaded' | 'loaded' = 'loading';
-
-  constructor(private db: AngularFireDatabase, private auth: AngularFireAuth) {
-    this.forIndex().subscribe(firstBills => {
-      this.status = 'partiallyLoaded';
-      this.billsCache.next(firstBills.reverse());
-      this.db.list('billing/bills', {
-        query: { orderByChild: 'id' }
-      }).subscribe(allBills => {
-        this.status = 'loaded';
-        this.billsCache.next(allBills.reverse());
-      });
-    });
-  }
-
-  private forIndex(): Observable<Bill[]> {
-    return this.db.list('billing/bills', {
-      query: { limitToLast: 1, orderByChild: 'id' }
-    });
+  constructor(private dataStoreService: DataStoreService) {
+    this.dataStoreService.loadData();
   }
 
   search(term: string): Observable<SearchResult<Bill>> {
-    return this.billsCache.map(allBills => {
-      const bills = allBills.filter(bill => {
+    return this.dataStoreService.getBillsStream().map(bills => {
+      const filteredBills = bills.filter(bill => {
         return (bill.address1 && bill.address1.startsWith(term)) ||
           (bill.address2 && bill.address2.startsWith(term)) ||
           (bill.address3 && bill.address3.startsWith(term)) ||
           (bill.address4 && bill.address4.startsWith(term));
       }).slice(0, 10);
-      return { term, list: bills, dbStatus: this.status };
+      return { term, list: filteredBills, dbStatus: this.dataStoreService.status };
     });
   }
 }
---
+++ src/app/bills/store/data-store-status.ts
@@ -0,0 +1,5 @@
+export type DataStoreStatus =
+  'loading' |
+  'loadedFromIDB' |
+  'shortListLoaded' |
+  'loaded';
