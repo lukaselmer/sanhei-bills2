e32d94541b51e39d25dc08537f19c79a99325387
Lukas Elmer <lukas.elmer@gmail.com>
2017-07-19 03:58:58 +0200

    Search case insensitive

--- src/app/bills/bills.service.ts
+++ src/app/bills/bills.service.ts
@@ -19,10 +19,10 @@ export class BillsService {
   search(term: string): Observable<SearchResult<Bill>> {
     return this.dataStoreService.getBillsStream().map(bills => {
       const filteredBills = bills.filter(bill => {
-        return (bill.address1 && bill.address1.startsWith(term)) ||
-          (bill.address2 && bill.address2.startsWith(term)) ||
-          (bill.address3 && bill.address3.startsWith(term)) ||
-          (bill.address4 && bill.address4.startsWith(term));
+        return (bill.address1 && bill.address1.toLowerCase().startsWith(term)) ||
+          (bill.address2 && bill.address2.toLowerCase().startsWith(term)) ||
+          (bill.address3 && bill.address3.toLowerCase().startsWith(term)) ||
+          (bill.address4 && bill.address4.toLowerCase().startsWith(term));
       }).slice(0, 10);
       return { term, list: filteredBills, dbStatus: this.dataStoreService.status };
     });
