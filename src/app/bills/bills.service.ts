ccab15a9576404cf2712da3127d064378fa998be
Lukas Elmer <lukas.elmer@renuo.ch>
2017-07-20 22:32:00 +0200

    Fix search

--- src/app/bills/bills.service.ts
+++ src/app/bills/bills.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/concatAll';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeAll';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/toArray';
-
 import { Observable } from 'rxjs/Observable';
 import { Bill } from './bill';
 import { BillMatcherFactory } from './search/bill-matcher.factory';
@@ -21,12 +23,15 @@ export class BillsService {
 
   search(term: string): Observable<SearchResult<Bill>> {
     const billMatcher = this.billMatcherFactory.createBillMatcher(term);
-    // workaround: the typings of mergeAll are wrong, see https://github.com/ReactiveX/rxjs/pull/2760
-    const billsAsObservable: Observable<Bill> = this.dataStore.getBillsStream().mergeAll() as any;
-    return billsAsObservable
-      .filter(bill => billMatcher.matches(bill))
-      .take(10)
-      .toArray()
+
+    return this.dataStore.getBillsStream()
+      .map(bills => {
+        return Observable.from(bills)
+          .filter(bill => billMatcher.matches(bill))
+          .take(10)
+          .toArray();
+      })
+      .concatAll()
       .map(filteredBills => this.wrapSearchResult(term, filteredBills));
   }
 
