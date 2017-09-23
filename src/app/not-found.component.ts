ab3932326d8e16604a6a951ed46d2c7dfc356427
Lukas Elmer <lukas.elmer@gmail.com>
2017-09-23 20:00:53 +0200

    Refactoring

--- src/app/not-found.component.ts
+++ src/app/not-found.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   template: `<h1>Page not found</h1>
-  <a [routerLink]="['/bills']">Home</a>`
+  <a href='/'>Home</a>`
 })
 export class PageNotFoundComponent { }
