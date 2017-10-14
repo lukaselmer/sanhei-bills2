e5be9a76803477e441a2d17dd761eeeca923d5c5
Lukas Elmer <lukas.elmer@gmail.com>
2017-10-14 22:24:00 +0200

    Instantiate amount matcher in factory

--- src/app/bills/search/bill-matcher.factory.ts
+++ src/app/bills/search/bill-matcher.factory.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Bill } from '../bill';
+import { AmountMatcher } from './matchers/amount-matcher';
 import { FullTextMatcher } from './matchers/full-text-matcher';
 import { IBillMatcher } from './matchers/i-bill-matcher';
 import { IDMatcher } from './matchers/id-matcher';
@@ -15,11 +16,20 @@ export class BillMatcherFactory {
       return this.createIDTextMatcher(id);
     }
 
+    if (term.startsWith('betrag:')) {
+      const id = parseFloat(term.split(':')[1].trim());
+      return this.createAmountTextMatcher(id);
+    }
+
     return this.createFullTextMatcher(term);
   }
 
   createIDTextMatcher(id: number) {
     return new IDMatcher(id);
+  }
+
+  createAmountTextMatcher(amount: number) {
+    return new AmountMatcher(amount);
   }
 
   createFullTextMatcher(term: string) {
