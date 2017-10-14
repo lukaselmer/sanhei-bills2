b5899597fd02ab169275f7f73e8a508e6c817add
Lukas Elmer <lukas.elmer@gmail.com>
2017-10-14 22:01:52 +0200

    Implement ID matcher

--- src/app/bills/search/bill-matcher.factory.spec.ts
+++ src/app/bills/search/bill-matcher.factory.spec.ts
@@ -1,6 +1,7 @@
 import { async } from '@angular/core/testing';
 import { BillMatcherFactory } from './bill-matcher.factory';
 import { FullTextMatcher } from './matchers/full-text-matcher';
+import { IDMatcher } from './matchers/id-matcher';
 
 describe('DataStoreService', () => {
   const service = new BillMatcherFactory();
@@ -11,6 +12,13 @@ describe('DataStoreService', () => {
       const instance = service.createBillMatcher({ term: 'some', limit: 10 });
       expect(instance instanceof FullTextMatcher).toBeTruthy();
       expect(service.createFullTextMatcher).toHaveBeenCalledWith('some');
+    });
+
+    it('creates an ID matcher', () => {
+      spyOn(service, 'createIDTextMatcher').and.callThrough();
+      const instance = service.createBillMatcher({ term: 'id: 32523', limit: 10 });
+      expect(instance instanceof IDMatcher).toBeTruthy();
+      expect(service.createIDTextMatcher).toHaveBeenCalledWith(32523);
     });
   });
 });
--- src/app/bills/search/bill-matcher.factory.ts
+++ src/app/bills/search/bill-matcher.factory.ts
@@ -2,12 +2,24 @@ import { Injectable } from '@angular/core';
 import { Bill } from '../bill';
 import { FullTextMatcher } from './matchers/full-text-matcher';
 import { IBillMatcher } from './matchers/i-bill-matcher';
+import { IDMatcher } from './matchers/id-matcher';
 import { SearchOptions } from './search-options';
 
 @Injectable()
 export class BillMatcherFactory {
   createBillMatcher(options: SearchOptions): IBillMatcher {
-    return this.createFullTextMatcher(options.term);
+    const term = options.term;
+
+    if (term.startsWith('id:')) {
+      const id = parseInt(term.split(':')[1].trim(), 10);
+      return this.createIDTextMatcher(id);
+    }
+
+    return this.createFullTextMatcher(term);
+  }
+
+  createIDTextMatcher(id: number) {
+    return new IDMatcher(id);
   }
 
   createFullTextMatcher(term: string) {
