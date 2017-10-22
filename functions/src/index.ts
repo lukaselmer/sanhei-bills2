6a129e49536deb1e7559dee1a48bbf52f3f1eace
Lukas Elmer <lukas.elmer@gmail.com>
2017-10-22 04:17:32 +0200

    Implement cloud function to set IDs automatically

---
+++ functions/src/index.ts
@@ -0,0 +1,52 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import { Bill } from './../../src/app/bills/bill';
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.database();
+let lastSetVersion = 0;
+
+/**
+ * There is a race condition in updateBillIds: it can happen that the same bill
+ * number is assigned to multiple bills by the system. It is very unlikely, since
+ * it is not expected that a user adds muliple bills within a few milliseconds.
+ * However, if this happens, the user can manually change the bill id in the UI.
+ */
+
+async function setHumanId(data: functions.database.DeltaSnapshot, nextHumanId: number) {
+  data.ref.child('humanId').set(nextHumanId);
+}
+
+async function setUid(data: functions.database.DeltaSnapshot, nextHumanId: number) {
+  const date = new Date();
+  const year = date.getUTCFullYear() % 2000;
+  const monthStr = `${date.getUTCMonth() + 1}`;
+  const monthPadded = monthStr.length === 1 ? `0${monthStr}` : monthStr;
+  const uidStr = `${year}${monthPadded}${nextHumanId}`;
+  data.ref.child('uid').set(+uidStr);
+}
+
+export const updateBillIds = functions.database.ref('billing/bills/{billId}').onCreate(event => {
+  const data = event.data;
+  const setIdPromise = data.ref.child('id').set(data.key);
+
+  return new Promise((resolve, reject) => {
+    db
+      .ref('billing/bills')
+      .orderByChild('humanId')
+      .limitToLast(1)
+      .once('value', snapshot => {
+        const val = snapshot.val();
+        const lastVersionInDb = val[Object.keys(val)[0]].humanId;
+        const nextHumanId = Math.max(lastSetVersion + 1, lastVersionInDb + 1);
+        lastSetVersion = nextHumanId;
+        data.ref.child('humanId').set(nextHumanId);
+        Promise.all([
+          setIdPromise,
+          setHumanId(data, nextHumanId),
+          setUid(data, nextHumanId)
+        ]).then(() => resolve())
+          .catch(error => reject(error));
+      });
+  });
+});
