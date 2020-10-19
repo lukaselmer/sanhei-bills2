58a546db7629159a966ebcaecf20c406d96bad05
Lukas Elmer <lukas.elmer@gmail.com>
2020-10-20 01:53:56 +0200

    Update app config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,11 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: 'AIzaSyBRJRYWoOxSOXEoXO4fxiPzaWXGmoaIeJI',
-    databaseURL: 'https://sanhei-bills.firebaseio.com',
-    storageBucket: 'sanhei-bills.appspot.com',
     authDomain: 'sanhei-bills.firebaseapp.com',
-    messagingSenderId: '1051872822136',
+    databaseURL: 'https://sanhei-bills.firebaseio.com',
     projectId: 'sanhei-bills',
+    storageBucket: 'sanhei-bills.appspot.com',
+    messagingSenderId: '1051872822136',
+    appId: '1:1051872822136:web:c5e81474612dd2abaf7e8d',
   },
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,10 +7,11 @@ export const environment = {
   production: false,
   firebase: {
     apiKey: 'AIzaSyDq-NEBhmKmrmLNzRi6JQL1wQh_FRA_rf0',
-    databaseURL: 'https://sanhei-bills-development.firebaseio.com',
-    storageBucket: 'sanhei-bills-development.appspot.com',
     authDomain: 'sanhei-bills-development.firebaseapp.com',
-    messagingSenderId: '1096089472440',
+    databaseURL: 'https://sanhei-bills-development.firebaseio.com',
     projectId: 'sanhei-bills-development',
+    storageBucket: 'sanhei-bills-development.appspot.com',
+    messagingSenderId: '1096089472440',
+    appId: '1:1096089472440:web:4ddf1707a69f636e59d34d',
   },
 }
