1fadaa43ccd0e252d9363acd7e9d9879edaa3baf
Lukas Elmer <lukas.elmer@gmail.com>
2024-12-14 12:16:44 +0100

    Upgrade angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,8 @@ const routes: Routes = [
 ]
 
 @NgModule({
+  // Make sure you configure setupTestingRouter, canceledNavigationResolution, paramsInheritanceStrategy, titleStrategy, urlUpdateStrategy, urlHandlingStrategy, and malformedUriErrorHandler in provideRouter or RouterModule.forRoot since these properties are now not part of the Router's public API
+  // https://angular.dev/api/router/RouterConfigOptions
   imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule],
 })
