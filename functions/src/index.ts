73290801771491e31fb70a7e749b19e95194db21
Lukas Elmer <lukas.elmer@gmail.com>
2024-12-15 00:43:21 +0100

    Update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,24 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 import { dateForUID } from './date-helper'
+import { onValueCreated } from 'firebase-functions/database'
 
 admin.initializeApp(functions.config().firebase)
 
 /**
  * There is a race condition in updateBillIds: it can happen that the same bill
  * number is assigned to multiple bills by the system. It is very unlikely, since
- * it is not expected that a user adds muliple bills within a few milliseconds.
+ * it is not expected that a user adds multiple bills within a few milliseconds.
  * However, if this happens, the user can manually change the bill id in the UI.
  */
 
-export const updateBillIds = functions
-  .region('europe-west6')
-  .database.ref('billing/bills/{billId}')
-  .onCreate(async (data: any) => {
+onValueCreated(
+  {
+    ref: 'billing/bills/{billId}',
+    region: 'europe-west6',
+  },
+  async (event) => {
+    const data = event.data
     if (data.val().humanId) return data.ref.update({ id: data.key })
 
     const snapshot = await admin
@@ -28,4 +32,5 @@ export const updateBillIds = functions
     const nextHumanId = lastVersionInDb + 1
     const uidStr = `${dateForUID()}${nextHumanId}`
     await data.ref.update({ id: data.key, humanId: nextHumanId, uid: +uidStr })
-  })
+  },
+)
